9ad4cc13f9c8cbf3f1ba914894f31181e37dcf02
Masum Billah <masumbillah6584@gmail.com>
2022-11-26 22:14:46 +0600

    fix some colors issue

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -34,70 +34,68 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className="">
-      <div className="navbar bg-primary text-white rounded-b">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+    <div className="navbar bg-base-200 text-black rounded-b">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              {navLink}
-            </ul>
-          </div>
-          <Link to="/" className="btn btn-ghost uppercase text-xl">
-            Marchentry
-          </Link>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navLink}
+          </ul>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal p-0">{navLink}</ul>
-        </div>
-        <div className="navbar-end">
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                <img src={user?.photoURL} alt="" />
-              </div>
-            </label>
-            <ul
-              tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <Link to="/" className="justify-between">
-                  {user?.displayName}
-                </Link>
-              </li>
-              <li>
-                <Link to="/">Dashboard</Link>
-              </li>
-              <li>
-                <Link to="/">Orders</Link>
-              </li>
-              <li>
-                <Link to="/">Booking</Link>
-              </li>
-              <li>
-                <button onClick={handleLogOut}>Log Out</button>
-              </li>
-            </ul>
-          </div>
+        <Link to="/" className="btn btn-ghost uppercase text-xl">
+          Marchentry
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">{navLink}</ul>
+      </div>
+      <div className="navbar-end">
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src={user?.photoURL} alt="" />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link to="/" className="justify-between">
+                {user?.displayName}
+              </Link>
+            </li>
+            <li>
+              <Link to="/">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/">Orders</Link>
+            </li>
+            <li>
+              <Link to="/">Booking</Link>
+            </li>
+            <li>
+              <button onClick={handleLogOut}>Log Out</button>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,7 +4,7 @@ const Button = ({ children }) => {
   console.log(children);
   return (
     <>
-      <button className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-600  sm:w-auto">
+      <button className="block w-full rounded bg-blue-700 px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-600  sm:w-auto">
         {children}
       </button>
     </>
