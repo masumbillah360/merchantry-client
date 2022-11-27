a2d42d89e5eb4f1dbc10751569be1ee0080b3421
Masum Billah <masumbillah6584@gmail.com>
2022-11-28 00:30:39 +0600

    added users profile on dashboard

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,91 +1,13 @@
-import axios from "axios";
-import React, { useContext, useEffect, useState } from "react";
-import { GoUnverified, GoVerified } from "react-icons/go";
-import { AuthContext } from "../../contexts/AuthProvider";
+import React from "react";
+import DashboardProfile from "./DashboardProfile/DashboardProfile";
 
 const Dashboard = () => {
-  const { user } = useContext(AuthContext);
-  const email = user?.email;
-  const [usersInfo, setUsersInfo] = useState(null);
-  useEffect(() => {
-    axios
-      .get(`http://localhost:8000/users/${email}`, {
-        headers: {
-          authorisation: `bearer ${localStorage.getItem("merchantry-token")}`,
-        },
-      })
-      .then((res) => {
-        console.log(res);
-        setUsersInfo(res.data);
-      })
-      .catch((err) => console.log(err));
-  }, [email]);
-
-  const handleVerify = (email) => {
-    console.log(email);
-    const userStatus = {
-      verifyStatus: "Pending",
-    };
-    console.log(userStatus);
-
-    fetch(`http://localhost:8000/users?email=${email}`, {
-      method: "PUT",
-      headers: {
-        "content-type": "application/json",
-        authorisation: `bearer ${localStorage.getItem("merchantry-token")}`,
-      },
-      body: JSON.stringify(userStatus),
-    })
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-  };
   return (
     <div>
-      Dash board{usersInfo?.name}
-      <div className="card full max-w-md mx-auto bg-base-100 shadow-xl">
-        <figure className="px-10 pt-10">
-          <img
-            src="https://placeimg.com/400/225/arch"
-            alt="Shoes"
-            className="rounded-full h-40 w-40"
-          />
-        </figure>
-        <div className="card-body items-center text-center">
-          <div className="flex justify-start gap-2 items-center">
-            <h1 className="text-lg font-bold">{usersInfo?.name}</h1>
-            {usersInfo?.verify && (
-              <span>
-                <GoVerified className="text-primary" />
-              </span>
-            )}
-          </div>
-
-          {usersInfo?.verify ? (
-            <span className="flex justify-start gap-2 items-center">
-              Verified:
-              <GoVerified className="text-primary" />
-            </span>
-          ) : (
-            <span className="flex justify-start gap-2 items-center">
-              Verified:
-              <GoUnverified className="text-error" />
-            </span>
-          )}
-          <p>{usersInfo?.email}</p>
-          <div className="card-actions">
-            {usersInfo?.verify ? (
-              <span className="btn btn-success ">Verified</span>
-            ) : (
-              <button
-                onClick={() => handleVerify(usersInfo?.email)}
-                className="btn btn-primary"
-              >
-                Verify Account
-              </button>
-            )}
-          </div>
-        </div>
-      </div>
+      <h1 className="text-primary font-bold text-center">
+        Welcome To DashBoard
+      </h1>
+      <DashboardProfile />
     </div>
   );
 };
---
+++ src/Pages/Dashboard/DashboardProfile/DashboardProfile.js
@@ -0,0 +1,110 @@
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+import { GoUnverified, GoVerified } from "react-icons/go";
+import { AuthContext } from "../../../contexts/AuthProvider";
+
+const DashboardProfile = () => {
+  const { user } = useContext(AuthContext);
+  const email = user?.email;
+  const [usersInfo, setUsersInfo] = useState(null);
+  const [userVerifyStatus, setUserVerifyStatus] = useState("");
+  console.log(userVerifyStatus);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/users/${email}`, {
+        headers: {
+          authorisation: `bearer ${localStorage.getItem("merchantry-token")}`,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        setUsersInfo(res?.data);
+        setUserVerifyStatus(res?.data?.verifyStatus);
+      })
+      .catch((err) => console.log(err));
+  }, [email, userVerifyStatus]);
+
+  const handleVerify = (email) => {
+    console.log(email);
+    const userStatus = {
+      verifyStatus: "Pending",
+    };
+    if (userVerifyStatus === "Pending") {
+      toast.success("Your Request has been Pending");
+      return;
+    }
+    fetch(`http://localhost:8000/users?email=${email}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+        authorisation: `bearer ${localStorage.getItem("merchantry-token")}`,
+      },
+      body: JSON.stringify(userStatus),
+    })
+      .then((res) => {
+        console.log(res);
+        if (res.ok) {
+          toast.success("Your Request Has Pending, Please Wait");
+          setUserVerifyStatus("Pending");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  const handleSuccessToast = () => {
+    toast.success("You Are Already Verified");
+  };
+  return (
+    <div>
+      <div className="card full max-w-md mx-auto bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+          <img
+            src={user?.photoURL}
+            alt="Shoes"
+            className="rounded-full h-40 w-40"
+          />
+        </figure>
+        <div className="card-body items-center text-center">
+          <div className="flex justify-start gap-2 items-center">
+            <h1 className="text-lg font-bold">{usersInfo?.name}</h1>
+            {usersInfo?.verify && (
+              <span>
+                <GoVerified className="text-primary" />
+              </span>
+            )}
+          </div>
+
+          {usersInfo?.verify ? (
+            <span className="flex justify-start gap-2 items-center">
+              Verified:
+              <GoVerified className="text-primary" />
+            </span>
+          ) : (
+            <span className="flex justify-start gap-2 items-center">
+              Verified:
+              <GoUnverified className="text-error" />
+            </span>
+          )}
+          <p>{usersInfo?.email}</p>
+          <div className="card-actions">
+            {usersInfo?.verify ? (
+              <button onClick={handleSuccessToast} className="btn btn-success ">
+                Verified
+              </button>
+            ) : (
+              <button
+                onClick={() => handleVerify(usersInfo?.email)}
+                className="btn btn-primary"
+              >
+                {userVerifyStatus || "Verify Account"}
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardProfile;
